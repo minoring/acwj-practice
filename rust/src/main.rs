b46d7b4de63c7fa6368bd5c00c2d201d7ed026c2
minoring <minho1350@gmail.com>
2020-12-17 20:13:25 +0900

    Move input to root, start rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::process::exit;
+
+pub static mut LINE: i32 = 1;
+pub static mut PUTBACK: char = '\n';
+
+unsafe fn init() {
+    LINE = 1;
+    PUTBACK = '\n';
+}
+
+// Print out a usage if started incorrectly
+fn usage(prog: &str) {
+    eprintln!("Usage: {} infile\n", prog);
+    exit(1);
+}
+
+fn main() -> std::io::Result<()> {
+    let argc = env::args().count();
+    let argv: Vec<String> = env::args().collect();
+
+    if argc != 2 {
+        usage(&argv[0]);
+    }
+
+    unsafe {
+        init();
+    }
+
+    let file = File::open(&argv[1])?;
+
+    Ok(())
 }
